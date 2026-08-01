8e0bb5fe0efb7aa2a4e53371c2cf821d1272b4af
aaravpandey198 <aaravpandey198@gmail.com>
2026-08-01 23:45:37 +0530

    Time: 59 ms (82.09%) | Memory: 19.3 MB (87.61%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -1,17 +1,9 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        l3=[]
-        l2=[]
-        l4=[]
-        
-        for i in range(int(len(nums)/2)):
-            l2.append(nums[i])
-            
-        for j in range(int(len(nums)/2),len(nums)):
-            l3.append(nums[j])
-        
-        for k in range(int(len(nums)/2)):
-            l4.append(l2[k])
-            l4.append(l3[k])
-        
-        return l4
+        ans = []
+
+        for i in range(n):
+            ans.append(nums[i])
+            ans.append(nums[i + n])
+
+        return ans
