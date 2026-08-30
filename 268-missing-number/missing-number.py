3c5e092adaf3d477449845d6cbc69c56839e1e62
aaravpandey198 <aaravpandey198@gmail.com>
2026-08-30 18:53:20 +0530

    Update missing-number.py

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -2,6 +2,7 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         #bit wise solution
         #xor operation
+        
         result = 0
         for i in range (len(nums)):
             result = result ^ i ^ nums[i]
