14453c7474c2e1e7ca484988a82463fa3071eae0
aaravpandey198 <aaravpandey198@gmail.com>
2026-09-19 12:32:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (27.32%) - LeetSync

---
+++ 31-next-permutation/next-permutation.py
@@ -0,0 +1,24 @@
+class Solution:
+    def nextPermutation(self, nums: list[int]) -> None:
+        n = len(nums)
+
+        gola_index = -1
+
+        for i in range(n - 1, 0, -1):
+            if nums[i] > nums[i - 1]:
+                gola_index = i - 1
+                break
+
+        if gola_index != -1:
+
+            swap_index = gola_index
+
+            for i in range(n - 1, gola_index, -1):
+                if nums[i] > nums[gola_index]:
+                    swap_index = i
+                    break
+
+            nums[gola_index], nums[swap_index] = \
+                nums[swap_index], nums[gola_index]
+
+        nums[gola_index + 1:] = reversed(nums[gola_index + 1:])
